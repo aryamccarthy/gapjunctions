d62fd0a55194bde2a618079fa9655cbd5ceda187
Arya McCarthy <arya.mccarthy@gmail.com>
2017-02-16 12:57:51 -0600

    Add interpolator (#3)

---
+++ gapjunctions/interpolate.py
@@ -0,0 +1,14 @@
+
+"""
+Interpolation algorithms using cubic Hermite polynomials.
+"""
+
+from __future__ import division, print_function, absolute_import
+
+import numpy as np
+from scipy.interpolate import KroghInterpolator
+
+def interpolate(x0, x1, m0, m1, t0, t1):
+    x = np.repeat([t0, t1], 2)
+    y = np.ravel(np.dstack(([x0, x1], [m0, m1])))
+    return KroghInterpolator(x, y)
