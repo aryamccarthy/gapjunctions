55df46a27a7a04cfb888900d1cb3bdf23b986e79
Arya McCarthy <arya.mccarthy@gmail.com>
2017-05-29 22:38:53 -0500

    Update interpolate.py

--- gapjunctions/interpolate.py
+++ gapjunctions/interpolate.py
@@ -8,6 +8,7 @@ from __future__ import division, print_function, absolute_import
 import numpy as np
 from scipy.interpolate import KroghInterpolator
 
+
 def interpolate(x0, x1, m0, m1, t0, t1):
     x = np.repeat([t0, t1], 2)
     y = np.ravel(np.dstack(([x0, x1], [m0, m1])))
